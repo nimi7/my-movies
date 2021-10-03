a6077679fbb001568fc03ee1770b6605ffed900f
nimi7 <62261707+nimi7@users.noreply.github.com>
2021-10-03 18:45:09 +0300

    loading

---
+++ src/loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+export default function Loading() {
+    return (
+        <div>
+            <div>
+                <img src='https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b6e0b072897469.5bf6e79950d23.gif' />
+            </div>
+        </div>
+    )
+}
